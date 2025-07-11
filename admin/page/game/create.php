20a6a1b8cea6831812bce1eff1f17b01abf50b2b
SaroeunTola <tolasweatboy@gmail.com>
2025-07-11 14:53:24 +0700

    fix

--- admin/page/game/create.php
+++ admin/page/game/create.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Games</title>
+    <title>Create Posts</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
--- admin/page/game/edit.php
+++ admin/page/game/edit.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Product</title>
+    <title>Edit Post</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
@@ -86,8 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <textarea name="meta_text" rows="3" required
                 class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"><?= htmlspecialchars($productData['meta_text']) ?></textarea>
                
-                   
-                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        
             </div>
             <!-- game_link -->
             <div class="mb-4">
